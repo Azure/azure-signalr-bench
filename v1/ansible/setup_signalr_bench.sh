d4b6ef3243899ccc07004d873274c8f13619cf52
hongjiang zhang <honzhan@microsoft.com>
2018-08-03 10:54:31 +0800

    use latest script

--- v1/ansible/setup_signalr_bench.sh
+++ v1/ansible/setup_signalr_bench.sh
@@ -5,7 +5,7 @@ ansible-playbook -i signalr_hosts install_config_go.yaml
 ansible-playbook -i signalr_hosts git_clone_websocket.yaml
 ansible-playbook -i signalr_hosts build_websocket.yaml
 ansible-playbook -i signalr_hosts git_clone_azuresignalrchatsample.yaml
-ansible-playbook -i signalr_hosts git_clone_signalr_auto_test_framework.yaml
+ansible-playbook -i signalr_hosts git_clone_azure_signalr_bench.yaml
 ansible-playbook -i signalr_hosts install_package.yaml
 ansible-playbook -i signalr_hosts install_kubectl.yaml
 ansible-playbook -i signalr_hosts build_azuresignalrsample.yaml # this step will fail, but it helps install dotnet, next yaml will build
