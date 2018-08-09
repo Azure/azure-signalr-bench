6de11797030c038e55d70d7404e9787db807beac
Hongjiang Zhang <honzhan@microsoft.com>
2018-08-09 10:05:04 +0000

    add the deployment for service

--- v1/start_service_on_multiple_vms.sh
+++ v1/start_service_on_multiple_vms.sh
@@ -2,4 +2,5 @@
 . ./multiple_vm_env.sh
 . ./build_launch_signalr_service.sh
 
+deploy_package_4_multiple_service_vm $raw_asrs_bin "$redis" "${service_vm_list}" $ssh_user $ssh_port
 launch_service_on_all_vms "$service_vm_list" $ssh_user $ssh_port
