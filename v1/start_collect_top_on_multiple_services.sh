654da2473cf52fc8c5aac37ca9eeb43a20a81c64
Hongjiang Zhang <honzhan@microsoft.com>
2018-08-09 15:11:40 +0000

    add default output folder

--- v1/start_collect_top_on_multiple_services.sh
+++ v1/start_collect_top_on_multiple_services.sh
@@ -2,11 +2,18 @@
 . ./func_env.sh
 . ./multiple_vm_env.sh
 
-timestamp=`date +%Y%m%d%H%M%S`
-top_foler=/tmp/services_tops_${timestamp}
-if [ ! -d $top_foler ]
+top_folder=""
+if [ $# -eq 1 ]
 then
-  mkdir $top_foler
+  top_folder="$1"
+else
+  timestamp=`date +%Y%m%d%H%M%S`
+  top_folder=/tmp/services_tops_${timestamp}
 fi
 
-collect_top_on_all_vms "$service_vm_list" $ssh_user $ssh_port $top_foler
+if [ ! -d $top_folder ]
+then
+  mkdir $top_folder
+fi
+
+collect_top_on_all_vms "$service_vm_list" $ssh_user $ssh_port $top_folder
