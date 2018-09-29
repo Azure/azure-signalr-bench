94533452dfe5a7b3488f55f2e344204301a90a9c
Unknown <wanl@microsoft.com>
2018-09-29 14:14:48 +0800

    Create new branch and initialize benchmark project

---
+++ SignalRServiceBenchmark/src/Microsoft.Azure.SignalR.Benchmark.Master/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Azure.SignalR.Benchmark.Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
