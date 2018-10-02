11a87f875069dbf64749bdd138e9c13b4c9705af
Hongjiang Zhang <honzhan@microsoft.com>
2018-10-02 07:52:26 +0000

    fix bug

--- v1/serversenteventsecho.py
+++ v1/serversenteventsecho.py
@@ -24,6 +24,27 @@ def serversenteventsechounit50_connection():
 def serversenteventsechounit100_connection():
    return serversentevent_connection[100]
 
+def serversenteventsechounit1_concurrentConnection():
+   return serversentevent_concurrentConnection[1]
+
+def serversenteventsechounit2_concurrentConnection():
+   return serversentevent_concurrentConnection[2]
+
+def serversenteventsechounit5_concurrentConnection():
+   return serversentevent_concurrentConnection[5]
+
+def serversenteventsechounit10_concurrentConnection():
+   return serversentevent_concurrentConnection[10]
+
+def serversenteventsechounit20_concurrentConnection():
+   return serversentevent_concurrentConnection[20]
+
+def serversenteventsechounit50_concurrentConnection():
+   return serversentevent_concurrentConnection[50]
+
+def serversenteventsechounit100_concurrentConnection():
+   return serversentevent_concurrentConnection[100]
+
 def serversenteventsechounit1(duration):
    #print sys._getframe().f_code.co_name
    connection=serversentevent_connection[1]
