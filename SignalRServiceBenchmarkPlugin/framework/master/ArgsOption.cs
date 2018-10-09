09b505212c6e83c5f2426efc54ad77dd2adbbd2d
Unknown <wanl@microsoft.com>
2018-10-09 16:25:27 +0800

    Add HelpText for all ArgsOption

--- SignalRServiceBenchmarkPlugin/framework/master/ArgsOption.cs
+++ SignalRServiceBenchmarkPlugin/framework/master/ArgsOption.cs
@@ -6,17 +6,17 @@ namespace Rpc.Master
     public class ArgsOption
     {
         // Rpc 
-        [Option("RpcPort", Required = false, Default = 5050, HelpText = "")]
+        [Option("RpcPort", Required = false, Default = 5050, HelpText = "Port to connect to the remote host")]
         public int RpcPort { get; set; }
         
-        [Option("SlaveList", Required = false, Separator = ',', Default = new string[]{"localhost"}, HelpText = "")]
+        [Option("SlaveList", Required = false, Separator = ',', Default = new string[]{"localhost"}, HelpText = "Target hosts to connect")]
         public IList<string> SlaveList { get; set; }
 
         // Log
-        [Option("LogName", Required = false, Default = "master-.log", HelpText = "")]
+        [Option("LogName", Required = false, Default = "master-.log", HelpText = "Log file name. Timestamp will insert into the position before dot.")]
         public string LogName { get; set; }
 
-        [Option("LogDirectory", Required = false, Default = ".", HelpText = "")]
+        [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored.")]
         public string LogDirectory { get; set; }
     }
 }
--- SignalRServiceBenchmarkPlugin/framework/slave/ArgsOption.cs
+++ SignalRServiceBenchmarkPlugin/framework/slave/ArgsOption.cs
@@ -6,17 +6,17 @@ namespace Rpc.Slave
     class ArgsOption
     {
         // Rpc
-        [Option("RpcPort", Required = false, Default = 5050, HelpText = "")]
+        [Option("RpcPort", Required = false, Default = 5050, HelpText = "Port to be conencted from remote hosts.")]
         public int RpcPort { get; set; }
 
-        [Option("HostName", Required = false, Default = "localhost", HelpText = "")]
+        [Option("HostName", Required = false, Default = "localhost", HelpText = "Host name.")]
         public string HostName{ get; set; }
 
         // Log
-        [Option("LogName", Required = false, Default = "slave-.log", HelpText = "")]
+        [Option("LogName", Required = false, Default = "slave-.log", HelpText = "Log file name. Timestamp will insert into the position before dot.")]
         public string LogName { get; set; }
 
-        [Option("LogDirectory", Required = false, Default = ".", HelpText = "")]
+        [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored.")]
         public string LogDirectory { get; set; }
     }
 }
