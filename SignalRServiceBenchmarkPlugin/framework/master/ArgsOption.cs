c2c1371efa06ce9d330d519c8790c9d0bf3f3a60
Unknown <wanl@microsoft.com>
2018-10-09 16:31:47 +0800

    Add explanations to default values.

--- SignalRServiceBenchmarkPlugin/framework/master/ArgsOption.cs
+++ SignalRServiceBenchmarkPlugin/framework/master/ArgsOption.cs
@@ -6,17 +6,17 @@ namespace Rpc.Master
     public class ArgsOption
     {
         // Rpc 
-        [Option("RpcPort", Required = false, Default = 5050, HelpText = "Port to connect to the remote host")]
+        [Option("RpcPort", Required = false, Default = 5050, HelpText = "Port to connect to the remote host. Default port is 5050.")]
         public int RpcPort { get; set; }
         
-        [Option("SlaveList", Required = false, Separator = ',', Default = new string[]{"localhost"}, HelpText = "Target hosts to connect")]
+        [Option("SlaveList", Required = false, Separator = ',', Default = new string[]{"localhost"}, HelpText = "Target hosts to connect. Default list is [localhost].")]
         public IList<string> SlaveList { get; set; }
 
         // Log
-        [Option("LogName", Required = false, Default = "master-.log", HelpText = "Log file name. Timestamp will insert into the position before dot.")]
+        [Option("LogName", Required = false, Default = "master-.log", HelpText = "Log file name. Timestamp will insert into the position before dot. Default name is 'master-.log'. The final file name will be 'master-123456789.log' for example.")]
         public string LogName { get; set; }
 
-        [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored.")]
+        [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored. Default directory is '.'.")]
         public string LogDirectory { get; set; }
     }
 }
--- SignalRServiceBenchmarkPlugin/framework/slave/ArgsOption.cs
+++ SignalRServiceBenchmarkPlugin/framework/slave/ArgsOption.cs
@@ -6,17 +6,17 @@ namespace Rpc.Slave
     class ArgsOption
     {
         // Rpc
-        [Option("RpcPort", Required = false, Default = 5050, HelpText = "Port to be conencted from remote hosts.")]
+        [Option("RpcPort", Required = false, Default = 5050, HelpText = "Port to be conencted from remote hosts. Default port is 5050.")]
         public int RpcPort { get; set; }
 
-        [Option("HostName", Required = false, Default = "localhost", HelpText = "Host name.")]
+        [Option("HostName", Required = false, Default = "localhost", HelpText = "Hostname. Default hostname is 'localhost'.")]
         public string HostName{ get; set; }
 
         // Log
-        [Option("LogName", Required = false, Default = "slave-.log", HelpText = "Log file name. Timestamp will insert into the position before dot.")]
+        [Option("LogName", Required = false, Default = "slave-.log", HelpText = "Log file name. Timestamp will insert into the position before dot. Default name is 'master-.log'. The final file name will be 'master-123456789.log', for example.")]
         public string LogName { get; set; }
 
-        [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored.")]
+        [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored. Default directory is '.'.")]
         public string LogDirectory { get; set; }
     }
 }
