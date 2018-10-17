29d1c09be58e15e227c347dd3630ba2ff85e4369
Wanpeng Li <wanl@microsoft.com>
2018-10-17 10:22:46 +0800

    Broadcast method is tested

---
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/MasterMethods/CollectConnectionId.cs
@@ -0,0 +1,25 @@
+﻿using Plugin.Base;
+using Rpc.Service;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Microsoft.Azure.SignalR.Benchmark.MasterMethods
+{
+    public class CollectConnectionId : IMasterMethod
+    {
+        public async Task Do(IDictionary<string, object> stepParameters, IDictionary<string, object> pluginParameters, IList<IRpcClient> clients)
+        {
+            Log.Information($"Collect connection ID...");
+
+            // Process on clients
+            var results = await Task.WhenAll(from client in clients select client.QueryAsync(stepParameters));
+
+            var connectionIds = new Dictionary<string, int>();
+            foreach (Dictionary<string, int> result in results) connectionIds.Union(result);
+        }
+    }
+}
