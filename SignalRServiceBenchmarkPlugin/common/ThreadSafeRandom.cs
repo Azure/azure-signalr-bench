92f1b1595e8e84c84e0916ed33161763279ec3ab
Wanpeng Li <wanl@microsoft.com>
2018-10-18 12:50:27 +0800

    Extension for shuffling list

---
+++ SignalRServiceBenchmarkPlugin/common/ThreadSafeRandom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Common
+{
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic] private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+}
