541781f995f273e34cca29b40cee67e5bfb58868
Wanpeng Li <wanl@microsoft.com>
2018-10-18 12:50:27 +0800

    Extension for shuffling list

--- SignalRServiceBenchmarkPlugin/common/Extensions.cs
+++ SignalRServiceBenchmarkPlugin/common/Extensions.cs
@@ -50,5 +50,18 @@ namespace Common
             var list = dict.Select(entry => $"{entry.Key} : {entry.Value}");
             return string.Join(Environment.NewLine, list);
         }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
     }
 }
