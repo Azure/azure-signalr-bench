4f3e06f3b6743f74272917eda48629d07ff3488c
Wanpeng Li <wanl@microsoft.com>
2018-10-18 12:49:56 +0800

    Change base send function interface

--- SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/SlaveMethods/BaseContinuousSendMethod.cs
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/SlaveMethods/BaseContinuousSendMethod.cs
@@ -12,11 +12,13 @@ namespace Plugin.Microsoft.Azure.SignalR.Benchmark.SlaveMethods
         protected async Task ContinuousSend<T, TKey, TValue>(
         T connection, IDictionary<TKey, TValue> data, 
         Func<T, IDictionary<TKey, TValue>, Task> f, 
-        TimeSpan duration, TimeSpan interval)
+        TimeSpan duration, TimeSpan interval, TimeSpan delayMin, TimeSpan delayMax)
         {
+            // TODO
             // Random delay in [1, interval) ms
             var rand = new Random();
-            var randomDelay = TimeSpan.FromMilliseconds(rand.Next((int)(interval.TotalMilliseconds - 1)) + 1);
+            //var randomDelay = TimeSpan.FromMilliseconds(rand.Next((int)(interval.TotalMilliseconds - 1)) + 1);
+            var randomDelay = TimeSpan.FromMilliseconds(rand.Next((int)delayMin.TotalMilliseconds, (int)delayMax.TotalMilliseconds));
             await Task.Delay(randomDelay);
 
             // Send message continuously
