761d8714b3ecabdca0428040e35ef07cdf9e5fce
Wanpeng Li <wanl@microsoft.com>
2018-10-19 16:05:26 +0800

    User custom serializer and deserializer

--- SignalRServiceBenchmarkPlugin/plugins/base/PluginInterface/IPlugin.cs
+++ SignalRServiceBenchmarkPlugin/plugins/base/PluginInterface/IPlugin.cs
@@ -9,5 +9,7 @@ namespace Plugin.Base
     // User can implement Iplugin to handle step in master and in slaves
     public interface IPlugin: IMasterStepHandler, ISlaveStepHandler
     {
+        string Serialize(IDictionary<string, object> data);
+        Dictionary<string, object> Deserialize(string input);
     }
 }
