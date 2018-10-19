e08c76ec63e9f47943d6c0ccbe02f842f9c03909
Wanpeng Li <wanl@microsoft.com>
2018-10-19 13:34:57 +0800

    Add "Wait" Method

---
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/MasterMethods/Wait.cs
@@ -0,0 +1,23 @@
+﻿using Plugin.Base;
+using Rpc.Service;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Microsoft.Azure.SignalR.Benchmark.MasterMethods
+{
+    public class Wait : IMasterMethod
+    {
+        public Task Do(IDictionary<string, object> stepParameters, IDictionary<string, object> pluginParameters, IList<IRpcClient> clients)
+        {
+            Log.Information($"Wait...");
+
+            // Process on clients
+            return Task.WhenAll(from client in clients select client.QueryAsync(stepParameters));
+
+        }
+    }
+}
