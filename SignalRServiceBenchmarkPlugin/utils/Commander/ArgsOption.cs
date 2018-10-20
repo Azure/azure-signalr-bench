d276b0bf42ea3a1427dc0bc877fb7a3237b63f84
Wanpeng Li <wanl@microsoft.com>
2018-10-21 03:34:56 +0800

    Add commander

---
+++ SignalRServiceBenchmarkPlugin/utils/Commander/ArgsOption.cs
@@ -0,0 +1,59 @@
+﻿using CommandLine;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commander
+{
+    // TODO: edit descriptions and default values
+    public class ArgsOption
+    {
+        // Remote
+        [Option("SlaveList", Required = false, Separator = ',', Default = new string[] { "localhost:5050", "localhost:6060" }, HelpText = "Target hosts to connect.")]
+        public IList<string> SlaveList { get; set; }
+
+        [Option("MasterHostname", Required = true, Default = ".", HelpText = "Hostname of master.")]
+        public string MasterHostname { get; set; }
+
+        [Option("AppServerHostname", Required = true, Default = ".", HelpText = "Hostname of app server.")]
+        public string AppServerHostname { get; set; }
+
+        [Option("Username", Required = true, Default = ".", HelpText = "Username of slaves.")]
+        public string Username { get; set; }
+
+        [Option("Password", Required = true, Default = ".", HelpText = "Password of slaves.")]
+        public string Password { get; set; }
+
+        // Log
+        [Option("LogName", Required = false, Default = "master-.log", HelpText = "Log file name. Timestamp will insert into the position before dot. If the name is 'master-.log'. The final file name will be 'master-123456789.log' for example.")]
+        public string LogName { get; set; }
+
+        [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored.")]
+        public string LogDirectory { get; set; }
+
+        [Option("LogTarget", Required = false, Default = LogTargetEnum.All, HelpText = "Log target. " +
+            "Options: All/File/Console." + " All: Output to file and console;" +
+            " Console: Output to console;" + " File: Output to file.")]
+        public LogTargetEnum LogTarget { get; set; }
+
+        // Project 
+        [Option("AppserverProject", Required = false, Default = ".", HelpText = "App server project.")]
+        public string AppserverProject { get; set; }
+
+        [Option("MasterProject", Required = false, Default = ".", HelpText = "Master project.")]
+        public string MasterProject { get; set; }
+
+        [Option("SlaveProject", Required = false, Default = ".", HelpText = "Slave project.")]
+        public string SlaveProject { get; set; }
+
+        [Option("AppserverTargetPath", Required = false, Default = ".", HelpText = "")]
+        public string AppserverTargetPath { get; set; }
+
+        [Option("MasterTargetPath", Required = false, Default = ".", HelpText = "")]
+        public string MasterTargetPath { get; set; }
+
+        [Option("SlaveTargetPath", Required = false, Default = ".", HelpText = "")]
+        public string SlaveTargetPath { get; set; }
+    }
+}
---
+++ SignalRServiceBenchmarkPlugin/utils/Commander/Program.cs
@@ -0,0 +1,120 @@
+﻿using CommandLine;
+using Common;
+using Medallion.Shell;
+using Renci.SshNet;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var argsOption = ParseArgs(args);
+
+            Util.CreateLogger(argsOption.LogDirectory, argsOption.LogName, argsOption.LogTarget);
+
+            // Publish dlls
+            var appserverExecutable = PubishExcutable(argsOption.AppserverProject);
+            var masterExecutable = PubishExcutable(argsOption.MasterProject);
+            var slaveExecutable = PubishExcutable(argsOption.SlaveProject);
+
+            // Copy executables
+            CopyExcutables(argsOption.Username, argsOption.Password,
+                argsOption.AppServerHostname, argsOption.MasterHostname, argsOption.SlaveList,
+                appserverExecutable, masterExecutable, slaveExecutable, 
+                argsOption.AppserverTargetPath, argsOption.MasterTargetPath, argsOption.SlaveTargetPath);
+
+            // Create Ssh clients
+            (var appServerSshClient, var masterSshClient, var slaveSshClients) 
+                = CreateSshClients(argsOption.Username, argsOption.Password, 
+                argsOption.AppServerHostname, argsOption.MasterHostname, argsOption.SlaveList);
+
+            // Connect to Vms
+            appServerSshClient.Connect();
+            masterSshClient.Connect();
+            slaveSshClients.ToList().ForEach(client => client.Connect());
+
+            // Run benchmark
+            RunBenchmark(appServerSshClient, masterSshClient, slaveSshClients,
+                 argsOption.AppserverTargetPath, argsOption.MasterTargetPath, argsOption.SlaveTargetPath);
+        }
+
+        private static void RunBenchmark(SshClient appServerSshClient, SshClient masterSshClient, IList<SshClient> slaveSshClients,
+            string appserverTargetPath, string masterTargetPath, string slaveTargetPat)
+        {
+            var appseverCommand = $"dotnet exec {appserverTargetPath}";
+            var masterCommand = $"dotnet exec {masterTargetPath}";
+            var slaveCommand = $"dotnet exec {slaveTargetPat}";
+
+            var appserverSshCommand = appServerSshClient.CreateCommand(appseverCommand);
+            var masterSshCommand = masterSshClient.CreateCommand(masterCommand);
+            var slaveSshCommands = (from client in slaveSshClients select client.CreateCommand(slaveCommand)).ToList();
+
+            var appserverAsyncResult = appserverSshCommand.BeginExecute();
+            var slaveAsyncResults = (from command in slaveSshCommands select command.BeginExecute()).ToList();
+
+            var masterResult = masterSshCommand.Execute();
+
+            _ = appserverSshCommand.EndExecute(appserverAsyncResult);
+            _ = from i in Enumerable.Range(0, slaveCommand.Count())
+                select slaveSshCommands[i].EndExecute(slaveAsyncResults[i]);
+        }
+
+        private static (SshClient, SshClient, IList<SshClient>) CreateSshClients(string username, string password,
+            string appserverHostname, string masterHostname, IList<string> slaveHostnames)
+        {
+            return (new SshClient(appserverHostname, username, password),
+                new SshClient(masterHostname, username, password), 
+                (from slave in slaveHostnames select new SshClient(slave, username, password)).ToList());
+        }
+
+        private static void CopyExcutables(string username, string password, 
+            string appserverHostname, string masterHostname, IList<string> slaveHostnames,
+            FileInfo appserverExecutable, FileInfo masterExecutable, FileInfo slaveExecutable,
+            string appserverTargetPath, string masterTargetPath, string slaveTargetPath)
+        {
+            // Copy executable to app server
+            var appServerScpClient = new ScpClient(appserverHostname, username, password);
+            appServerScpClient.Upload(appserverExecutable, appserverTargetPath);
+
+            // Copy executable to master
+            var masterScpClient = new ScpClient(masterHostname, username, password);
+            masterScpClient.Upload(masterExecutable, masterTargetPath);
+
+            // Copy excutable to slaves
+            var slaveScpClients = from slave in slaveHostnames
+                                  select new ScpClient(slave, username, password);
+            foreach (var slaveScpClient in slaveScpClients) slaveScpClient.Upload(slaveExecutable, slaveTargetPath);
+        }
+
+        private static FileInfo PubishExcutable(string projectPath)
+        {
+            var baseName = "publish";
+            _ = Command.Run("dotnet", $"publish -o {baseName}".Split(' '), o => o.WorkingDirectory(projectPath));
+            _ = Command.Run("zip", $"-r publish.zip ./{baseName}/*".Split(' '), o => o.WorkingDirectory(projectPath));
+
+            return new FileInfo(Path.Combine(projectPath, $"{baseName}.zip"));
+        }
+
+        private static ArgsOption ParseArgs(string[] args)
+        {
+            Log.Information($"Parse arguments...");
+            var argsOption = new ArgsOption();
+            var result = Parser.Default.ParseArguments<ArgsOption>(args)
+                .WithParsed(options => argsOption = options)
+                .WithNotParsed(error =>
+                {
+                    Log.Error($"Error in parsing arguments: {error}");
+                    throw new ArgumentException("Error in parsing arguments.");
+                });
+            return argsOption;
+        }
+    }
+}
