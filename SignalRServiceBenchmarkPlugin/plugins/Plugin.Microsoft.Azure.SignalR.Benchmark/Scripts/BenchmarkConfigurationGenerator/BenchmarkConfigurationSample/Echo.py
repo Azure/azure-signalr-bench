0fce985be644cd43ebc92da7e10b31ca7d81b87b
Wanpeng Li <wanl@microsoft.com>
2018-10-25 17:21:39 +0800

    Update configuration generator

--- SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/BenchmarkConfigurationSample/Echo.py
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/BenchmarkConfigurationSample/Echo.py
@@ -4,30 +4,39 @@ import Common
 type_ = 'echo'
 total_connection = 20000
 wait_time = 5 * 1000
-duration = 10 * 1000
+duration = 60 * 1000
 interval = 1 * 1000
 statistic_interval = 1 * 1000
-message_size = 40
+message_size = 2 * 1024
 remainder_begin = 0
-remainder_end = 1
-remainder_end_dx = 3
+remainder_end_dx = 5000
+remainder_end_base = 5000
 concurrent_connection = 100
 protocol = 'json'
 transport_type = 'Websockets'
 types = [type_]
-pipeline = [
-    init_statistics_collector(type_) + init_statistics_collector(type_),
-    collect_statistics(type_, statistic_interval, './counters.txt'),
+
+pre_send = [
+    init_statistics_collector(type_),
+    collect_statistics(type_, statistic_interval, './counters_{}.txt'.format(type_)),
     create_connection(type_, total_connection, Common.hub_url, protocol, transport_type),
     start_connection(type_, concurrent_connection),
-    echo(type_, duration, interval, remainder_begin, remainder_end, total_connection, message_size),
-    wait(type_, wait_time),
-    echo(type_, duration, interval, remainder_begin, remainder_end + remainder_end_dx, total_connection, message_size),
-    wait(type_, wait_time),
+]
+
+sending = []
+for remainder_end in range(remainder_end_base, total_connection + 1, remainder_end_dx):
+    sending += [
+        echo(type_, duration, interval, remainder_begin, remainder_end, total_connection, message_size),
+        wait(type_, wait_time)
+    ]
+
+post_sending = [
     stop_collector(type_),
     stop_connection(type_),
     dispose_connection(type_)
 ]
+
+pipeline = pre_send + sending + post_sending
 
 Config = Common.set_config(types, pipeline)
 
