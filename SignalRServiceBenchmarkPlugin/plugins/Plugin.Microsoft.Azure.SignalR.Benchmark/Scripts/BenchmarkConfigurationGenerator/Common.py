8561afbe54aa55d7d5eba13c2502acaee905bc5d
Wanpeng Li <wanl@microsoft.com>
2018-10-26 13:15:44 +0800

    Update scripts

--- SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/Common.py
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/Common.py
@@ -1,7 +1,7 @@
 import BenchmarkConfigurationStep
 
 module_name = 'Plugin.Microsoft.Azure.SignalR.Benchmark.SignalRBenchmarkPlugin, Plugin.Microsoft.Azure.SignalR.Benchmark'
-hub_url = "http://wanleastusagents953034586appsvrdns0.eastus.cloudapp.azure.com:5050/signalrbench"
+hub_url = "http://wanleastusagents953034586AppSvrDNS0.eastus.cloudapp.azure.com:5050/signalrbench,http://wanleastusagents953034586AppSvrDNS1.eastus.cloudapp.azure.com:5050/signalrbench,http://wanleastusagents953034586AppSvrDNS2.eastus.cloudapp.azure.com:5050/signalrbench,http://wanleastusagents953034586AppSvrDNS3.eastus.cloudapp.azure.com:5050/signalrbench,http://wanleastusagents953034586AppSvrDNS4.eastus.cloudapp.azure.com:5050/signalrbench,http://wanleastusagents953034586AppSvrDNS5.eastus.cloudapp.azure.com:5050/signalrbench,http://wanleastusagents953034586AppSvrDNS6.eastus.cloudapp.azure.com:5050/signalrbench,http://wanleastusagents953034586AppSvrDNS7.eastus.cloudapp.azure.com:5050/signalrbench"
 
 Config = {
     BenchmarkConfigurationStep.Key['Types']: None,
