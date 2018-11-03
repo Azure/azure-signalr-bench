5aa351eda2c5ad5d716772b0099df3420f6b44c4
Hongjiang Zhang <honzhan@microsoft.com>
2018-11-03 15:48:29 +0000

    add more logic

--- Scripts/jenkins_functions.sh
+++ Scripts/jenkins_functions.sh
@@ -13,7 +13,7 @@ function set_global_env() {
    export JenkinsRootPath="$Jenkins_Workspace_Root"
    export ScriptWorkingDir=$Jenkins_Workspace_Root/${relative_dir}/Scripts                     # folders to find all scripts
    export CurrentWorkingDir=$Jenkins_Workspace_Root/${relative_dir}/v2/JenkinsScript/     # workding directory
-   export CommandWorkingDir=$Jenkins_Workspace_Root/SignalRServiceBenchmarkPlugin/utils/Commander
+   export CommandWorkingDir=$Jenkins_Workspace_Root/${relative_dir}/SignalRServiceBenchmarkPlugin/utils/Commander
 ############# those configurations are shared in Jenkins folder #####
    export AgentConfig=$JenkinsRootPath'/agent.yaml'
    export PrivateIps=$JenkinsRootPath'/privateIps.yaml'
@@ -72,25 +72,163 @@ then
 fi
 }
 
+function prepare_result_folder_4_scenario()
+{
+   local tag=$1
+   local Transport=$2
+   local MessageEncoding=$3
+   local Scenario=$4
+   ## take care of the gen of this folder which is also required by gen_html.sh
+   local scenario_folder=${tag}_${Transport}_${MessageEncoding}_${Scenario}
+   export env_statistic_folder=${ScriptWorkingDir}/${result_root}/${scenario_folder}
+   
+   mkdir -p ${env_statistic_folder}
+}
+
+function run_all_units() {
+ local user=$1
+ local passwd="$2"
+ local service
+ local signalrServiceName
+ for service in $bench_serviceunit_list
+ do
+   cd $ScriptWorkingDir
+   ConnectionString="" # set it to be invalid first
+   signalrServiceName="atpf"`date +%H%M%S`
+   create_asrs $DogFoodResourceGroup $signalrServiceName $Sku $service
+   if [ "$ConnectionString" == "" ]
+   then
+     echo "Skip the running on SignalR service unit'$service' since it was failed to create"
+     continue
+   fi
+
+   run_benchmark $service $user "$passwd" "$ConnectionString"
+   
+   delete_signalr_service $signalrServiceName $DogFoodResourceGroup
+ done
+}
+
+
+function run_benchmark() {
+  local unit=$1
+  local user=$2
+  local passwd="$3"
+  local connectStr="$4"
+  local tag="unit"$unit
+  local Scenario
+  local Transport
+  local MessageEncoding
+  
+  for Scenario in $bench_scenario_list
+  do
+      for Transport in $bench_transport_list
+      do
+         for MessageEncoding in $bench_encoding_list
+         do
+            prepare_result_folder_4_scenario $tag $Transport $MessageEncoding $Scenario
+            local k8s_result_dir=$env_statistic_folder
+            ## TODO start collecting top
+            cd $ScriptWorkingDir
+            . ./func_env.sh
+            . ./kubectl_utils.sh
+            local service_name=$(extract_servicename_from_connectionstring $ConnectionString)
+            if [ "$service_name" != "" ]
+            then
+               nohup sh collect_pod_top.sh $service_name $k8s_result_dir &
+               collect_pod_top_pid=$!
+               if [ "$g_nginx_ns" != "" ]
+               then
+                  nohup sh collect_nginx_top.sh $service_name $g_nginx_ns $k8s_result_dir &
+                  collect_nginx_top_pid=$!
+               fi     
+            fi
+            ## TODO generate configuration
+            run_and_gen_report $tag $Scenario $Transport $MessageEncoding $user $passwd "$connectStr" $k8s_result_dir
+            if [ "$service_name" != "" ]
+            then
+               if [ "$collect_pod_top_pid" != "" ]
+               then
+         # kill the process if it is alive
+                 local a=`ps -o pid= -p $collect_pod_top_pid`
+                 if [ "$a" != "" ]
+                 then
+                    kill $collect_pod_top_pid
+                 fi
+               fi
+               if [ "$collect_nginx_top_pid" != "" ]
+               then
+                 local a=`ps -o pid= -p $collect_nginx_top_pid`
+                 if [ "$a" != "" ]
+                 then
+                    kill $collect_nginx_top_pid
+                 fi
+               fi
+   ############# copy pod log ############
+               copy_syslog $service_name $k8s_result_dir
+               get_nginx_log $service_name "$g_nginx_ns" $k8s_result_dir
+               get_k8s_pod_status $service_name $k8s_result_dir
+            fi
+            ## TODO reboot ASRS
+         done
+      done
+  done
+
+}
+
+function run_and_gen_report() {
+  local tag=$1
+  local Scenario=$2
+  local Transport=$3
+  local MessageEncoding=$4
+  local user=$5
+  local passwd="$6"
+  local connectionString="$7"
+  local outputDir="$8"
+  #TODO dummy values
+  local connection=3000
+  local concurrentConnection=300
+  local send=1000
+  
+  run_command $user $passwd $connectionString $outputDir
+  cd $ScriptWorkingDir
+     #### generate the connection configuration for HTML ####
+cat << EOF > configs/cmd_4_${MessageEncoding}_${Scenario}_${tag}_${Transport}
+connection=${connection}
+connection_concurrent=${concurrentConnection}
+send=${send}
+bench_config_endpoint="$serverUrl"
+EOF
+
+  ## gen_html.sh requires bench_name_list, bench_codec_list, and bench_type_list
+  export bench_name_list="$Scenario"
+  export bench_codec_list="$MessageEncoding"
+  export bench_type_list="${tag}_${Transport}"
+  sh gen_html.sh $connectionString
+}
+
 function run_command() {
   local user=$1
   local passwd="$2"
+  local connectionString="$3"
+  local outputDir="$4"
   cd $ScriptWorkingDir
   local master=`python extract_ip.py -i $PrivateIps -q master`
   local appserver=`python extract_ip.py -i $PrivateIps -q appserver`
   local slaves=`python extract_ip.py -i $PrivateIps -q slaves`
   cd $CommandWorkingDir
   dotnet run -- --RpcPort=5555 --SlaveList="$slaves" --MasterHostname="$master" --AppServerHostnames="$appserver" \
-         --Username="$user" --Password="$passwd" \
+         --Username=$user --Password=$passwd \
          --AppserverProject="/home/wanl/executables/appserver" \
          --MasterProject="/home/wanl/executables/master" \
          --SlaveProject="/home/wanl/executables/slave" \
-         --AppserverTargetPath="/home/wanl/appserver.zip" --MasterTargetPath="/home/wanl/master.zip" \
-         --SlaveTargetPath="/home/wanl/slave.zip" \
+         --AppserverTargetPath="/home/${user}/appserver.tgz" --MasterTargetPath="/home/${user}/master.tgz" \
+         --SlaveTargetPath="/home/${user}/slave.tgz" \
          --BenchmarkConfiguration="/home/wanl/benchmarkConfiguration/echo.yaml" \
-         --BenchmarkConfigurationTargetPath="/home/wanl/signalr.yaml" \
-         --AzureSignalRConnectionString="Endpoint=https://signalr-wanl-benchmark-v3.service.signalr.net;AccessKey=4DB3v7sgkv+5mz8e+plPjA/JZPCYOBV4pDIJLH7tgng=;Version=1.0;"
+         --BenchmarkConfigurationTargetPath="/home/${user}/signalr.yaml" \
+         --AzureSignalRConnectionString="$connectionString"
+  sshpass -p ${passwd} scp -o StrictHostKeyChecking=no -o LogLevel=ERROR ${user}@${master}:/home/${user}/counters.txt ${outputDir}/
 }
+
 ## exit handler to remove resource group ##
 # global env:
 # CurrentWorkingDir, ServicePrincipal, AgentConfig, VMMgrDir
