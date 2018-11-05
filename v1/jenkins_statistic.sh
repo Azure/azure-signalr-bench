bf98b4426dca77f74f29cd2eb09c5c4d0bd58e38
Hongjiang Zhang <honzhan@microsoft.com>
2018-11-05 02:51:08 +0000

    update

--- v1/jenkins_statistic.sh
+++ v1/jenkins_statistic.sh
@@ -15,6 +15,9 @@ function publish_html() {
 if [ -e jenkins_stat_env.sh ]
 then
   . ./jenkins_stat_env.sh
+  export env_g_http_base=$env_g_http_base
+  export env_g_nginx_root_dir=$env_g_nginx_root_dir
+  export nginx_server_dns=$env_g_dns
 else
   echo "Specify jenkins_stat_env.sh which contains required parameters"
   exit 1
@@ -30,4 +33,4 @@ create_html
 
 publish_html
 
-sh send_mail.sh $env_g_nginx_root_dir/$env_g_root
+sh send_mail.sh $env_g_nginx_root_dir/$env_g_root/index.html
