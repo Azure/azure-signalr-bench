caccb8fad54d159f74de396c30a25514e1e63e00
Hongjiang Zhang <honzhan@microsoft.com>
2018-11-12 06:13:36 +0000

    add jenkins job configure to get schedule information

---
+++ Scripts/ListAllJenkinsTrigger.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FindAllConfig() {
+  local job trigger normalTrigger
+  for i in `find ~/.jenkins/jobs -iname "config.xml"`
+  do
+    job=`echo $i|awk -F / '{print $6}'`
+    trigger=`python FindTrigger.py -i $i`
+    if [ "$trigger" != "" ]
+    then
+      normalTrigger=`echo "$trigger"|tr '\n' ' '`
+      echo "$job,$normalTrigger"
+    fi
+  done
+}
+
+FindAllConfig
