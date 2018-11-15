da87bf67956031bf830f7e7b6b2a65aa53bb4516
Wanpeng Li <wanl@microsoft.com>
2018-11-15 12:46:58 +0800

    Comment

--- SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/SlaveMethods/SendToGroupBase.cs
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/SlaveMethods/SendToGroupBase.cs
@@ -1,21 +1,57 @@
-﻿using Plugin.Base;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
+using Plugin.Base;
 
 namespace Plugin.Microsoft.Azure.SignalR.Benchmark.SlaveMethods
 {
-    class SendToGroupBase : ISlaveMethod
-    {
-        public Task<IDictionary<string, object>> Do(IDictionary<string, object> stepParameters, IDictionary<string, object> pluginParameters)
-        {
-            
-        }
+    //public class SendToGroupBase : ISlaveMethod
+    //{
+    //    protected string type;
+    //    protected long duration;
+    //    protected long interval;
+    //    protected int messageSize;
+    //    protected int totalConnection;
+    //    protected int groupCount;
+    //    protected int GroupLevelRemainderBegin;
+    //    protected int GroupLevelRemainderEnd;
+    //    protected int GroupInternalRemainderBegin;
+    //    protected int GroupInternalRemainderEnd;
+    //    protected int GroupInternalModulo;
 
-        protected virtual void LoadParameters()
-        {
+    //    public Task<IDictionary<string, object>> Do(IDictionary<string, object> stepParameters, IDictionary<string, object> pluginParameters)
+    //    {
+    //        try
+    //        {
+    //            Log.Information($"Send to group...");
 
-        }
-    }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            var message = $"Fail to send to group: {ex}";
+    //            Log.Error(message);
+    //            throw;
+    //        }
+    //    }
+
+    //    protected virtual void LoadParameters(IDictionary<string, object> stepParameters)
+    //    {
+    //        // Get parameters
+    //        stepParameters.TryGetTypedValue(SignalRConstants.Type, out type, Convert.ToString);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.Duration, out duration, Convert.ToInt64);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.Interval, out interval, Convert.ToInt64);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.MessageSize, out messageSize, Convert.ToInt32);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.ConnectionTotal, out totalConnection, Convert.ToInt32);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.GroupCount, out groupCount, Convert.ToInt32);
+
+    //        // Group Mode
+    //        stepParameters.TryGetTypedValue(SignalRConstants.GroupLevelRemainderBegin, out GroupLevelRemainderBegin, Convert.ToInt32);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.GroupLevelRemainderEnd, out GroupLevelRemainderEnd, Convert.ToInt32);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.GroupInternalRemainderBegin, out GroupInternalRemainderBegin, Convert.ToInt32);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.GroupInternalRemainderEnd, out GroupInternalRemainderEnd, Convert.ToInt32);
+    //        stepParameters.TryGetTypedValue(SignalRConstants.GroupInternalModulo, out GroupInternalModulo, Convert.ToInt32);
+    //    }
+    //}
 }
