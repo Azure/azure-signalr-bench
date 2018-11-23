283d2bc075101ed014d447b38bcffab95412cc50
hongjiang zhang <honzhan@microsoft.com>
2018-11-23 04:05:01 +0000

    add AspNet SignalR Server App

---
+++ SignalRServiceBenchmarkPlugin/utils/AspNetServer/Startup.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatRoom.Startup))]
+
+namespace ChatRoom
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapAzureSignalR(this.GetType().FullName);
+            //GlobalHost.TraceManager.Switch.Level = System.Diagnostics.SourceLevels.All;
+        }
+    }
+}
