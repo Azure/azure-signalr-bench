ccdd1cc0075b07bacbfdd4dee7e432d49a5e72aa
Hongjiang Zhang <honzhan@microsoft.com>
2018-11-29 23:07:02 +0800

    add selfhost AspNetServer

---
+++ SignalRServiceBenchmarkPlugin/utils/AspNetSelfhostServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace AspNetSelfhostServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8009"))
+            {
+                Console.WriteLine("Server running at http://localhost:8009/");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SignalRServiceBenchmarkPlugin/utils/AspNetSelfhostServer/Startup.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+using System.Diagnostics;
+
+[assembly: OwinStartup(typeof(AspNetSelfhostServer.Startup))]
+namespace AspNetSelfhostServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapAzureSignalR(this.GetType().FullName);
+            GlobalHost.TraceManager.Switch.Level = SourceLevels.Information;
+        }
+    }
+}
