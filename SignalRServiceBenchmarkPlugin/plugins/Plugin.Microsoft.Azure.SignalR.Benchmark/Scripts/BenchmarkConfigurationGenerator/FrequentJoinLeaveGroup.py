260a38ce0691f927063b0ccea514b343d5f49aa0
Hongjiang Zhang <honzhan@microsoft.com>
2019-01-31 16:45:03 +0800

    refactor the script

--- SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/FrequentJoinLeaveGroup.py
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/FrequentJoinLeaveGroup.py
@@ -1,28 +1,18 @@
 from Util.BenchmarkConfigurationStep import *
 from Util import TemplateSetter, ConfigSaver, CommonStep
 from Util.Common import *
+import BaseScenario as ParentClass
 
+class FrequentJoinLeaveGroup(ParentClass.BaseScenario):
+    def __init__(self, sending_config, scenario_config, connection_config, statistics_config, constant_config, useAspNet=0):
+        super().__init__(sending_config, scenario_config, connection_config, statistics_config, constant_config, useAspNet)
 
-class FrequentJoinLeaveGroup:
-    def __init__(self, sending_config, scenario_config, connection_config, statistics_config, constant_config):
-        self.sending_config = sending_config
-        self.scenario_config = scenario_config
-        self.statistics_config = statistics_config
-        self.connection_config = connection_config
-        self.constant_config = constant_config
-
-    def generate_config(self):
-        pre_sending = CommonStep.pre_sending_steps(self.scenario_config.type, self.connection_config,
-                                                   self.statistics_config, self.scenario_config, self.constant_config)
-        pre_sending += [register_callback_record_latency(self.scenario_config.type)]
-
-        post_sending = CommonStep.post_sending_steps(self.scenario_config.type)
-
+    def BuildSending(self):
         remainder_end_dx = self.scenario_config.step
 
         arg_type = ArgType()
 
-        sending = []
+        self.sending = []
 
         for epoch in range(0, self.scenario_config.step_length):
 
@@ -38,14 +28,14 @@ class FrequentJoinLeaveGroup:
 
             # conditional stop and reconnect
             if epoch > 0:
-                sending += CommonStep.conditional_stop_and_reconnect_steps(sending, self.scenario_config,
+                self.sending += CommonStep.conditional_stop_and_reconnect_steps(self.sending, self.scenario_config,
                                                                            self.constant_config,
                                                                            self.connection_config)
 
             if self.scenario_config.group_config_mode == arg_type.group_config_mode_group:
                 group_member = 3 if self.scenario_config.group_type == arg_type.group_big \
                     else self.scenario_config.connections // self.scenario_config.group_count
-                sending += [frequent_join_leave_group_group_mode(self.scenario_config.type,
+                self.sending += [frequent_join_leave_group_group_mode(self.scenario_config.type,
                                                                  self.sending_config.duration,
                                                                  self.sending_config.interval,
                                                                  self.sending_config.message_size,
@@ -55,7 +45,7 @@ class FrequentJoinLeaveGroup:
                                                                  self.scenario_config.connections //
                                                                  self.scenario_config.group_count)]
             else:
-                sending += [frequent_join_leave_group_connection_mode(self.scenario_config.type,
+                self.sending += [frequent_join_leave_group_connection_mode(self.scenario_config.type,
                                                                       self.sending_config.duration,
                                                                       self.sending_config.interval,
                                                                       self.sending_config.message_size,
@@ -63,12 +53,13 @@ class FrequentJoinLeaveGroup:
                                                                       self.scenario_config.group_count,
                                                                       0, remainder_end,
                                                                       self.scenario_config.connections)]
-            sending += [
+            self.sending += [
                 wait(self.scenario_config.type, self.constant_config.wait_time)
             ]
 
-        pipeline = pre_sending + sending + post_sending
-
-        config = TemplateSetter.set_config(self.constant_config.module, [self.scenario_config.type], pipeline)
-
-        ConfigSaver.save_yaml(config, self.constant_config.config_save_path)
+    def generate_config(self):
+        super().BuildCommonPreSending()
+        super().BuildRegRecordLatency()
+        super().BuildPostSending()
+        self.BuildSending()
+        super().GenerateConfig()
--- SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/SendToClient.py
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Scripts/BenchmarkConfigurationGenerator/SendToClient.py
@@ -1,36 +1,16 @@
 from Util.BenchmarkConfigurationStep import *
 from Util import TemplateSetter, ConfigSaver, CommonStep
+import BaseScenario as ParentClass
 
+class SendToClient(ParentClass.BaseScenario):
+    def __init__(self, sending_config, scenario_config, connection_config, statistics_config, constant_config, useAspNet=0):
+        super().__init__(sending_config, scenario_config, connection_config, statistics_config, constant_config, useAspNet)
 
-class SendToClient:
-    def __init__(self, sending_config, scenario_config, connection_config, statistics_config, constant_config):
-        self.sending_config = sending_config
-        self.scenario_config = scenario_config
-        self.statistics_config = statistics_config
-        self.connection_config = connection_config
-        self.constant_config = constant_config
-
-    def generate_config(self):
-        pre_sending = CommonStep.pre_sending_steps(self.scenario_config.type, self.connection_config,
-                                                   self.statistics_config, self.scenario_config, self.constant_config)
-        pre_sending += [register_callback_record_latency(self.scenario_config.type)]
-
-        pre_sending += [collect_connection_id(self.scenario_config.type)]
-        pre_sending += [wait(self.scenario_config.type, self.constant_config.wait_time)]
-        pre_sending += [reconnect(self.scenario_config.type,
-                                  self.scenario_config.connections,
-                                  self.connection_config.url,
-                                  self.connection_config.protocol,
-                                  self.connection_config.transport,
-                                  self.scenario_config.concurrent)]
-        pre_sending += [wait(self.scenario_config.type, self.constant_config.wait_time)]
-        pre_sending += [collect_connection_id(self.scenario_config.type)]
-        post_sending = CommonStep.post_sending_steps(self.scenario_config.type)
-
+    def BuildSending(self):
         remainder_begin = 0
         remainder_end_dx = self.scenario_config.step
 
-        sending = []
+        self.sending = []
         for epoch in range(0, self.scenario_config.step_length):
             remainder_end = self.scenario_config.base_step + epoch * remainder_end_dx
 
@@ -39,18 +19,25 @@ class SendToClient:
 
             # conditional stop and reconnect
             if epoch > 0:
-                sending += CommonStep.conditional_stop_and_reconnect_steps(sending, self.scenario_config, self.constant_config,
+                self.sending += CommonStep.conditional_stop_and_reconnect_steps(self.sending, self.scenario_config, self.constant_config,
                                             self.connection_config)
 
-            sending += [
+            self.sending += [
                 send_to_client(self.scenario_config.type, self.scenario_config.connections,
                                self.sending_config.duration, self.sending_config.interval, remainder_begin,
                                remainder_end, self.scenario_config.connections, self.sending_config.message_size),
                 wait(self.scenario_config.type, self.constant_config.wait_time)
             ]
 
-        pipeline = pre_sending + sending + post_sending
+    def generate_config(self):
+        super().BuildCommonPreSending()
+        super().BuildRegRecordLatency()
+        super().BuildCollectConnectionId()
+        super().BuildConstantWait()
+        super().BuildReconnect()
+        super().BuildConstantWait()
+        super().BuildCollectConnectionId()
+        super().BuildPostSending()
+        self.BuildSending()
+        super().GenerateConfig()
 
-        config = TemplateSetter.set_config(self.constant_config.module, [self.scenario_config.type], pipeline)
-
-        ConfigSaver.save_yaml(config, self.constant_config.config_save_path)
