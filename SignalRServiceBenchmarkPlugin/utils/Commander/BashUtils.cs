a02ced10d0257e827e4d9447b5009bf8db1f4958
Hongjiang Zhang <honzhan@microsoft.com>
2019-02-11 23:25:53 +0800

    use self-created file upload instead of SSH.NET scpclient

---
+++ SignalRServiceBenchmarkPlugin/utils/Commander/BashUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Commander
+{
+    class BashUtils
+    {
+        public static (int, string) Bash(string cmd, bool wait = true, bool handleRes = false, bool captureConsole = false)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            var result = "";
+            var errCode = 0;
+            if (wait == true)
+            {
+                if (captureConsole)
+                {
+                    while (!process.StandardOutput.EndOfStream)
+                    {
+                        Console.WriteLine(process.StandardOutput.ReadLine());
+                    }
+                }
+                else
+                {
+                    result = process.StandardOutput.ReadToEnd();
+                }
+                process.WaitForExit();
+                errCode = process.ExitCode;
+            }
+
+            if (handleRes == true)
+            {
+                if (errCode != 0)
+                {
+                    Console.WriteLine($"Handle result ERR {errCode}: {result}");
+                }
+            }
+
+            return (errCode, result);
+        }
+    }
+}
