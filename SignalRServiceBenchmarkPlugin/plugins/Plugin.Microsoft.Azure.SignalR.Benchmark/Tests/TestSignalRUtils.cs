e02d73f815ae49e7e9b590d9caecd6d6df459962
Hongjiang Zhang <honzhan@microsoft.com>
2019-02-12 11:15:05 +0800

    add a test case

---
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/Tests/TestSignalRUtils.cs
@@ -0,0 +1,21 @@
+﻿using Plugin.Microsoft.Azure.SignalR.Benchmark;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Azure.SignalR.Benchmark.Tests
+{
+    public class TestSignalRUtils
+    {
+        [Fact]
+        public void TestTryGetBatchMode()
+        {
+            var dic = new Dictionary<string, object>();
+            SignalRUtils.TryGetBatchMode(
+                dic,
+                out string batchConfigMode,
+                out int batchWaitMilliSeconds,
+                out SignalREnums.BatchMode mode);
+            Assert.Equal(batchConfigMode, SignalRConstants.DefaultBatchMode);
+        }
+    }
+}
