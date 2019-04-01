40d814c3a206c0835efb56c140aa0f673e0b370f
Hongjiang Zhang <honzhan@microsoft.com>
2019-04-01 17:14:05 +0800

    fix a bug

--- SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/MasterMethods/ReconnectBase.cs
+++ SignalRServiceBenchmarkPlugin/plugins/Plugin.Microsoft.Azure.SignalR.Benchmark/SignalRPlugin/MasterMethods/ReconnectBase.cs
@@ -14,9 +14,6 @@ namespace Plugin.Microsoft.Azure.SignalR.Benchmark.MasterMethods
         {
             // Get parameters
             stepParameters.TryGetTypedValue(SignalRConstants.ConnectionTotal, out int connectionTotal, Convert.ToInt32);
-            stepParameters.TryGetTypedValue(SignalRConstants.HubUrls, out string hubUrl, Convert.ToString);
-            stepParameters.TryGetTypedValue(SignalRConstants.TransportType, out string transportType, Convert.ToString);
-            stepParameters.TryGetTypedValue(SignalRConstants.HubProtocol, out string hubProtocol, Convert.ToString);
             stepParameters.TryGetTypedValue(SignalRConstants.ConcurrentConnection, out int concurrentConnection, Convert.ToInt32);
 
             // Prepare configuration for each clients
@@ -24,14 +21,8 @@ namespace Plugin.Microsoft.Azure.SignalR.Benchmark.MasterMethods
             {
                 int currentConcurrentConnection = Util.SplitNumber(concurrentConnection, i, clients.Count);
                 (int beg, int end) = Util.GetConnectionRange(connectionTotal, i, clients.Count);
-                var data = new Dictionary<string, object>(stepParameters)
-                {
-                    { SignalRConstants.HubUrls, hubUrl },
-                    { SignalRConstants.TransportType, transportType },
-                    { SignalRConstants.HubProtocol, hubProtocol },
-                    // Make sure concurrent connection is at least 1
-                    { SignalRConstants.ConcurrentConnection, currentConcurrentConnection > 0 ? currentConcurrentConnection : 1}
-                };
+                var data = new Dictionary<string, object>(stepParameters);
+                data[SignalRConstants.ConcurrentConnection] = currentConcurrentConnection > 0 ? currentConcurrentConnection : 1;
                 // Add method and type
                 PluginUtils.AddMethodAndType(data, stepParameters);
                 return new { Client = client, Data = data };
