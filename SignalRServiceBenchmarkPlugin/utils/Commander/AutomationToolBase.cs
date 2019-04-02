93c02933aa25d86e395b7a125676664c71bc6988
Hongjiang Zhang <honzhan@microsoft.com>
2019-04-02 09:50:59 +0800

    fix a bug of using tar

--- SignalRServiceBenchmarkPlugin/utils/Commander/AutomationToolBase.cs
+++ SignalRServiceBenchmarkPlugin/utils/Commander/AutomationToolBase.cs
@@ -195,11 +195,10 @@ namespace Commander
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                 {
-                    var tar = Command.Run("tar", new string[] { "zcvf", $"{localAppServerLogPath}.tgz", $"{localAppServerLogPath}" },
-                    //var tar = Command.Run("tar", new string[] { "-C", _appserverLogDirPath, "zcvf", $"{localAppServerLogPath}.tgz", $"{host}_{_appLogFileName}" },
+                    //var tar = Command.Run("tar", new string[] { "zcvf", $"{localAppServerLogPath}.tgz", $"{localAppServerLogPath}" },
+                    var tar = Command.Run("tar", new string[] { "-C", _appserverLogDirPath, "-zcvf", $"{localAppServerLogPath}.tgz", $"{localAppServerLogPath}" },
                     o => o.WorkingDirectory(_appserverLogDirPath));
-                    Log.Information($"tar zcvf {localAppServerLogPath}.tgz {localAppServerLogPath}");
-                    Log.Information($"tar -C {_appserverLogDirPath} zcvf {localAppServerLogPath}.tgz {host}_{_appLogFileName}");
+                    Log.Information($"tar -C {_appserverLogDirPath} -zcvf {localAppServerLogPath}.tgz {localAppServerLogPath}");
                     tar.Wait();
                     if (!tar.Result.Success)
                     {
