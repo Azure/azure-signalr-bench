d6d1cb976833862a86b532b52cd11404a9053751
Hongjiang Zhang <honzhan@microsoft.com>
2019-04-15 15:22:19 +0800

    fix the issue of counter normalization OOM killed

--- Scripts/normalize_counters.py
+++ Scripts/normalize_counters.py
@@ -10,6 +10,35 @@ def normalize(input):
             data.append(json.loads(line))
    return data 
 
+def in_memory_normalize(input):
+   # may be OOM killed
+   r = normalize(args.input)
+   jr = ast.literal_eval(json.dumps(r, sort_keys=True, indent=2))
+   nr = str(jr).replace("'", "\"")
+   print(nr)
+
+def streaming_normalize(input):
+   data = []
+   # get the total line number
+   totalLine = sum(1 for line in open(input))
+   if totalLine > 1:
+        print("[\n")
+   #
+   with open(input) as f:
+        curLine = 0
+        for line in f:
+            curLine = curLine + 1
+            a=json.loads(line)
+            jr = ast.literal_eval(json.dumps(a, sort_keys=True, indent=2))
+            nr = str(jr).replace("'", "\"")
+            print(nr)
+            if curLine < totalLine:
+               print(",")
+            else:
+               print("]")
+   return data
+
+
 if __name__=="__main__":
    parser = argparse.ArgumentParser()
    parser.add_argument("-i", "--input", help="Specify the input Yaml file")
@@ -17,7 +46,5 @@ if __name__=="__main__":
    if args.input is None:
       print("Input file is not specified!")
    else:
-      r = normalize(args.input)
-      jr = ast.literal_eval(json.dumps(r, sort_keys=True, indent=2))
-      nr = str(jr).replace("'", "\"")
-      print(nr)
+      #in_memory_normalize(args.input)
+      streaming_normalize(args.input)
