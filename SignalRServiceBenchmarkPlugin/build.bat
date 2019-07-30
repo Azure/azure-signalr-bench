d5f886fe2fe8a1742b37ef93bc3924c43d94fbe3
hongjiang zhang <honzhan@microsoft.com>
2019-07-30 15:23:13 +0800

    separate handling of korebuild

--- SignalRServiceBenchmarkPlugin/build.bat
+++ SignalRServiceBenchmarkPlugin/build.bat
@@ -1,6 +1,3 @@
 cd framework\rpc
 call build.bat
 cd ..\..
-
-@ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0run.ps1' default-build %*; exit $LASTEXITCODE"
---
+++ SignalRServiceBenchmarkPlugin/korebuild.sh
@@ -0,0 +1,6 @@
+set -euo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+Call "sync" between "chmod" and execution to prevent "text file busy" error in Docker (aufs)
+chmod +x "$DIR/run.sh"; sync
+"$DIR/run.sh" default-build "$@"
