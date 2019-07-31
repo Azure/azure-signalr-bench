38887bf1d1eb81fa9597f8fbe393899be469d2d2
Hongjiang Zhang <honzhan@microsoft.com>
2019-07-31 09:39:17 +0800

    remove the missing 'slave'

--- SignalRServiceBenchmarkPlugin/framework/agent/ArgsOption.cs
+++ SignalRServiceBenchmarkPlugin/framework/agent/ArgsOption.cs
@@ -10,16 +10,16 @@ namespace Rpc.Agent
         [Option("RpcPort", Required = false, Default = 7000, HelpText = "Port to be conencted from remote hosts.")]
         public int RpcPort { get; set; }
 
-        [Option("PidFile", Required = false, Default = "slave-pid.txt")]
+        [Option("PidFile", Required = false, Default = "agent-pid.txt")]
         public string PidFile { get; set; }
 
         [Option("HostName", Required = false, Default = "localhost", HelpText = "Hostname.")]
         public string HostName{ get; set; }
 
         // Log
-        [Option("LogName", Required = false, Default = "slave-.log", HelpText = "Log file name. " + 
-            "Timestamp will insert into the position before dot. If the name is 'slave-.log'. " + 
-            " The final file name will be 'slave-123456789.log', for example.")]
+        [Option("LogName", Required = false, Default = "agent-.log", HelpText = "Log file name. " +
+            "Timestamp will insert into the position before dot. If the name is 'agent-.log'. " +
+            " The final file name will be 'agent-123456789.log', for example.")]
         public string LogName { get; set; }
 
         [Option("LogDirectory", Required = false, Default = ".", HelpText = "Directory in which the log will be stored.")]
