88ff66f3c2eef46f551962a4ac72edc30b93798d
Hongjiang Zhang <honzhan@microsoft.com>
2019-10-22 11:37:35 +0800

    fix runtime error
    Endpoint Routing does not support 'IApplicationBuilder.UseMvc(...)'. To use
    'IApplicationBuilder.UseMvc' set 'MvcOptions.EnableEndpointRouting =
    false' inside 'ConfigureServices(...).

--- src/signalr/Internals/InternalStartup.cs
+++ src/signalr/Internals/InternalStartup.cs
@@ -18,7 +18,10 @@ namespace Plugin.Microsoft.Azure.SignalR.Benchmark.Internals
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(mvcOption =>
+            {
+                mvcOption.EnableEndpointRouting = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
