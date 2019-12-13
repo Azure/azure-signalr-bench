1e337145aa4459614842b7f26eb04c5693669212
hongjiang zhang <honzhan@microsoft.com>
2019-12-13 10:08:07 +0800

    add script to truncate the jenkins log file
    It is used to free disk space when it is full.

---
+++ scripts/clean_jenkins_joblogs_when_disk_full.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function clean_any_job()
+{
+        local i j
+        cd $HOME/.jenkins/jobs
+        pushd `pwd`
+        for i in `du -s *|sort -k 1 -n -r|head -n 1|awkk '{print $2}'`
+        do
+                cd $i/builds
+                pushd `pwd`
+                for j in `du -s *|sort -k 1 -n -r|head -n 10|awk '{print $2}'`
+                do
+                        truncate -s 1024 $j/log
+                done
+                popd
+        done
+        popd
+}
+
+clean_any_job
