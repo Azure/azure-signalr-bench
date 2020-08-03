36497ac5e2767da5fc8a5d5d298805ac3542ec35
bjqian <bjqian100@qq.com>
2020-08-03 11:18:33 +0800

    Biqian/init (#54)
    * add init.sh
    
    * add init.sh
    
    * make subscription optional , add cloud
    
    * fix
    
    * tmp commit
    
    * clean
    
    * tmp
    
    * tmp
    
    * add publish and deployment process of portal, coordinator..
    
    * clean
    
    * add redis deployment, create workspace when initing
    
    Co-authored-by: Binjie Qian <biqian@microsoft.com>

---
+++ Initialize/common.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function init_common() {
+    echo "init naming conventions and configs"
+    PREFIX_PERF="${PREFIX}perf"
+    RESOURCE_GROUP="${PREFIX_PERF}rg"
+    STORAGE_ACCOUNT="${PREFIX_PERF}sa"
+    KEYVAULT="${PREFIX_PERF}kv"
+    KUBERNETES_SEVICES="${PREFIX_PERF}aks"
+    PORTAL_IP_NAME="${PREFIX_PERF}ip"
+    KV_SA_ACCESS_KEY="sa-accessKey"
+    KV_KUBE_CONFIG="kube-config"
+    SA_SHARE="perf"
+    PORTAL_DNS="${PREFIX_PERF}-portal"
+
+    if [[ ! -z $CLOUD ]]; then
+        az cloud set -n $CLOUD
+    fi
+
+    if [[ ! -z $SUBSCTIPTION ]]; then
+        az account set -s $SUBSCTIPTION
+    else
+        SUBSCTIPTION=$(az account show --query "id" -o tsv)
+    fi
+}
+
+function init_aks_group() {
+    echo "init aks configs"
+    AKS_RESOURCE_GROUP=$(az aks show -g $RESOURCE_GROUP -n $KUBERNETES_SEVICES --query nodeResourceGroup -o tsv)
+    az aks get-credentials -g $RESOURCE_GROUP -n $KUBERNETES_SEVICES -a
+}
+
+function throw_if_empty() {
+    local name="$1"
+    local value="$2"
+    if [[ -z "$value" ]]; then
+        echo "ERROR: Parameter $name cannot be empty." 1>&2
+        exit -1
+    fi
+}
+
+function replace() {
+    local placeholder=$1
+    local content=$2
+    content=$(echo "${content////\\/}")
+    sed -e "s/$placeholder/$content/g"
+}
--- Initialize/publish.sh
+++ Initialize/publish.sh
@@ -2,11 +2,130 @@
 set -e
 trap "exit" INT
 
-## Used to compile and publish the project
-DIR=$(cd `dirname $0` && cd ../src/Pods && pwd)
+DIR=$(cd $(dirname $0) && pwd)
+source $DIR/common.sh
 
-cd $DIR/Portal
-rm -rf  publish
-echo "start to publish the Portal"
-dotnet publish --self-contained true -r linux-x64 -c release -o publish /p:useapphost=true
+PREFIX=""
 
+function print_usage() {
+    cat <<EOF
+Command 
+    $(basename $0)
+Arguments
+   --prefix|-p                          [Requied] Used to distingush your perf resources with others'
+   --subscription|-s                    [Optional] The subscriton used to create resources
+   --cloud|-c                           [Optional] The cloud used to create resources
+   --all|-a                             [Optional] publish portal,coordinator and compiler
+   --portal                             [Optional] publish portal
+   --coordinator                        [Optional] publish coordinator
+   --compiler                           [Optional] publish compiler
+   --server                             [Optional] publish compiler
+   --client                             [Optional] publish compiler
+   --help|-h                            Print help
+EOF
+}
+
+while [[ "$#" > 0 ]]; do
+    key="$1"
+    shift
+    case $key in
+    --prefix | -p)
+        PREFIX="$1"
+        shift
+        ;;
+    --cloud | -c)
+        CLOUD="$1"
+        shift
+        ;;
+    --subscription | -s)
+        SUBSCTIPTION="$1"
+        shift
+        ;;
+    --portal)
+        PORTAL=true
+        ;;
+    --coordinator)
+        COORDINATOR=true
+        ;;
+    --compiler)
+        COMPILER=true
+        ;;
+    --appserver)
+        APPSERVER=true
+        ;;
+    --client)
+        CLIENT=true
+        ;;
+    --redis)
+        REDIS=true
+        ;;
+    --all | -a)
+        ALL=true
+        ;;
+    --help | -h)
+        print_usage
+        exit
+        ;;
+    *)
+        echo "ERROR: Unknow argument '$key'" 1>&2
+        print_usage
+        exit -1
+        ;;
+    esac
+done
+
+function publish() {
+    Pod=$1
+    cd $DIR/../src/Pods/$Pod
+    rm -rf publish
+    echo "start to publish $Pod"
+    dotnet publish -r linux-x64 -c release -o publish /p:useapphost=true
+    cd publish && zip -r ${Pod}.zip *
+    echo "create dir:$Pod"
+    az storage directory create -n "manifest/$Pod" --account-name $STORAGE_ACCOUNT -s $SA_SHARE
+    az storage file upload --account-name $STORAGE_ACCOUNT -s $SA_SHARE --source $Pod.zip -p manifest/$Pod
+    echo "upload $Pod succeeded"
+}
+
+throw_if_empty "prefix" $PREFIX
+
+init_common
+init_aks_group
+
+if [[ $ALL || $PORTAL ]]; then
+    publish Portal
+    cd $DIR/yaml/portal
+    PORTAL_IP=$(az network public-ip show -n $PORTAL_IP_NAME -g $RESOURCE_GROUP --query "ipAddress" -o tsv)
+    cat load-balancer-service.yaml | replace RESOURCE_GROUP_PLACE_HOLDER $RESOURCE_GROUP | replace PORTAL_IP_PLACE_HOLDER $PORTAL_IP | kubectl apply -f -
+    kubectl apply -f portal.yaml
+    domain=$(az network public-ip show -n $PORTAL_IP_NAME -g $RESOURCE_GROUP --query dnsSettings.fqdn -o tsv)
+    echo "portal domain: $domain "
+fi
+
+if [[ $ALL || $COORDINATOR ]]; then
+    publish Coordinator
+    cd $DIR/yaml/coordinator
+    kubectl apply -f coordinator.yaml
+fi
+
+if [[ $ALL || $COMPILER ]]; then
+    publish Compiler
+    cd $DIR/yaml/compiler
+    kubectl apply -f compiler.yaml
+fi
+
+if [[ $ALL || $APPSERVER ]]; then
+    publish AppServer
+fi
+
+if [[ $ALL || $CLIENT ]]; then
+    publish Client
+fi
+
+if [[ $ALL || $REDIS ]]; then
+    ##This redis has only one instance. Change this to cluster mode later
+    cd $DIR/yaml/redis
+    kubectl apply -f redis-master-deployment.yaml
+    kubectl apply -f redis-master-service.yaml
+    echo "redis dns inside cluster: redis-master "
+fi
