694f49c74e1b20ffb3d7696da06463fa86a535ff
Binjie Qian <biqian@microsoft.com>
2020-09-09 16:24:35 +0800

    fix namespace

--- src/Pods/AppServer/MessageClientHolder.cs
+++ src/Pods/AppServer/MessageClientHolder.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AppServer
+namespace Azure.SignalRBench.AppServer
 {
     public class MessageClientHolder
     {
