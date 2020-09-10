f656f54e0ea450479542b5c3e084b2cc59637d84
Binjie Qian <biqian@microsoft.com>
2020-09-10 13:22:49 +0800

    add scenario state

---
+++ src/Pods/Client/IScenarioState.cs
@@ -0,0 +1,24 @@
+﻿using Azure.SignalRBench.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.SignalRBench.Client
+{
+    public interface IScenarioState
+    {
+        public void SetSenario(SetScenarioParameters setScenarioParameters);
+
+        public Task StartSenario(StartScenarioParameters startScenarioParameters);
+
+        public Task StopSenario(StopScenarioParameters stopScenario);
+
+        public void SetClientRange(SetClientRangeParameters setClientRangeParameters);
+
+        public Task StartClientConnections(StartClientConnectionsParameters startClientConnectionsParameters);
+
+        public Task StopClientConnections(StopClientConnectionsParameters StopClientConnectionsParameters);
+
+    }
+}
--- src/Pods/Client/Program.cs
+++ src/Pods/Client/Program.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Azure.SignalRBench.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Azure.SignalRBench.Client
@@ -9,7 +14,26 @@ namespace Azure.SignalRBench.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+              .AddEnvironmentVariables()
+              .Build();
+            return Host.CreateDefaultBuilder(args)
+                 .ConfigureLogging((ILoggingBuilder logging) =>
+                 {
+                     logging.ClearProviders();
+                     logging.AddConsole();
+                     logging.AddProvider(new BlobLoggerProvider(configuration[Constants.EnvVariableKey.StorageConnectionStringKey], Roles.Clients, configuration[Constants.EnvVariableKey.PodNameStringKey]));
+                 })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MessageClientHolder>();
+                    services.AddSingleton<IScenarioState, ScenarioState>();
+                });
         }
     }
 }
