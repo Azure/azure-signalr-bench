40df8016301482340e3c48247526f66cc715d4b9
wanl <wanl@microsoft.com>
2020-10-26 15:36:16 +0800

    update

--- deploy/vsts_trigger_jenkins_perf_test.sh
+++ deploy/vsts_trigger_jenkins_perf_test.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-
 function vsts_trigger_jenkins_perf_test {
     # do NOT support scenario list
     # only support one variable changes
 
-    local group_types=$1
-    local run_indexes=$2
+    local group_types=(`echo $1`)  # list
+    local run_indexes=(`echo $2`) # list
     local build_url=$3
     local user_name=$4
     local api_token=$5
     local asrs_location=$6
-    local units=$7
-    local scenarios=$8
-    local transport_types=$9
-    local protocols=${10}
-    local message_sizes=${11}
+    local units=(`echo $7`) # list
+    local scenarios=(`echo $8`)  # list
+    local transport_types=(`echo $9`)  # list
+    local protocols=(`echo ${10}`)  # list
+    local message_sizes=(`echo ${11}`) # list
     local vm_location=${12}
     local vm_image=${13}
     local vnet_resource_group=${14}
     local vnet_name=${15}
     local vnet_subnet=${16}
-    local client_vm_count=${17}
-    local server_vm_count=${18}
+    local client_vm_count=(`echo ${17}`)  # list
+    local server_vm_count=(`echo ${18}`)  # list
     local build_type=${19}
     local release_name=${20}
     local release_id=${21}
