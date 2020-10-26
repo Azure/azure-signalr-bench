3f38a664d4966cc1f53fdc97db22959772cfeed1
wanl <wanl@microsoft.com>
2020-10-26 13:03:47 +0800

    add vsts script

---
+++ deploy/vsts_trigger_jenkins_perf_test.sh
@@ -0,0 +1,48 @@
+vsts_trigger_jenkins_perf_test () {
+    # do NOT support scenario list
+    # only support one variable changes
+
+    local group_types=$1
+    local run_indexes=$2
+    local build_url=$3
+    local user_name=$4
+    local api_token=$5
+    local asrs_location=$6
+    local units=$7
+    local scenarios=$8
+    local transport_types=$9
+    local protocols=$10
+    local message_sizes=$11
+    local vm_location=$12
+    local vm_image=$13
+    local vnet_resource_group=$14
+    local vnet_name=$15
+    local vnet_subnet=$16
+    local client_vm_count=$17
+    local server_vm_count=$18
+    local build_type=$19
+    local release_name=$20
+    local release_id=$21
+
+    for ms in ${message_sizes[*]}; do \
+    for ts in ${transport_types[*]}; do \
+    for gp in ${group_types[*]}; do \
+    for i in ${run_indexes[*]}; do curl  $build_url \
+    --user $user_name:$api_token \
+    --data ASRSLocation=$asrs_location \
+    --data bench_serviceunit_list=${units[i]} \
+    --data bench_scenario_list=$scenarios \
+    --data bench_transport_list=$ts \
+    --data bench_encoding_list=$protocols \
+    --data sendToClientMsgSize=$ms \
+    --data VMLocation=$vm_location \
+    --data GlobalVmImageId=$vm_image \
+    --data BenchServerGroup=$vnet_resource_group \
+    --data VnetName=$vnet_name \
+    --data SubnetName=$vnet_subnet \
+    --data clientVmCount=${client_vm_count[i]} \
+    --data serverVmCount=${server_vm_count[i]} \
+    --data build_type=$19 \
+    --data GroupTypeList=$gp \
+    --data build_title_extra="Release Name: ${release_name} ID: ${release_id}" ; done; done;
+}
