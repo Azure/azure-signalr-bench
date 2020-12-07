12119b64dac816632943a5f2aae524d4c8c41849
Binjie Qian <biqian@microsoft.com>
2020-12-07 19:51:00 +0800

    fix format

--- src/Pods/Client/MessageClientHolder.cs
+++ src/Pods/Client/MessageClientHolder.cs
@@ -112,7 +112,8 @@ namespace Azure.SignalRBench.Client
 
         private async Task SetScenario(CommandMessage commandMessage)
         {
-            try{
+            try
+            {
                 var param = JsonConvert.SerializeObject(commandMessage.Parameters);
                 _logger.LogInformation($"Start to set scenario: {param}");
                 var setSenarioParameters = commandMessage.Parameters?.ToObject<SetScenarioParameters>();
