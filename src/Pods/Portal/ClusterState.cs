bd96b06fcc592c32cb881280d17082aff1a7896f
Binjie Qian <biqian@microsoft.com>
2021-01-28 17:14:58 +0800

    add cert

--- src/Pods/Portal/ClusterState.cs
+++ src/Pods/Portal/ClusterState.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Azure.Security.KeyVault.Secrets;
 using Azure.SignalRBench.Common;
@@ -18,21 +19,26 @@ namespace Portal
         public bool PPEEnabled { get; private set; }
 
         public string PPELocation { get; private set; } = "";
+        public X509Certificate2 AuthCert { get; private set; }
 
         public async Task Init()
         {
             var locationTask = _secretClient.GetSecretAsync(PerfConstants.KeyVaultKeys.LocationKey);
             var ppeTask = _secretClient.GetSecretAsync(PerfConstants.KeyVaultKeys.PPELocationKey);
+            var certTask = _secretClient.GetSecretAsync(PerfConstants.KeyVaultKeys.EntryptCert);
             Location = (await locationTask).Value.Value;
             try
             {
                 PPEEnabled = (await ppeTask).Value != null;
                 PPELocation = (await ppeTask).Value.Value;
+                var base64 = (await certTask).Value.Value;
+                AuthCert= new X509Certificate2( Convert.FromBase64String(base64));
             }
             catch (Exception)
             {
                 // ignored
             }
+            
         }
     }
 }
