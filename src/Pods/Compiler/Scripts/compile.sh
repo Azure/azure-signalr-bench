efd21516d2b755ddf7939a467e2a2ae1ebf78347
Binjie Qian <biqian@microsoft.com>
2021-04-02 13:30:27 +0800

    update compile.sh

--- src/Pods/Compiler/Scripts/compile.sh
+++ src/Pods/Compiler/Scripts/compile.sh
@@ -1,64 +1,87 @@
 #!/bin/bash
 set -e
-
+## only one bash should be run at the same time
 DIR=$(cd $(dirname $0) && pwd)
 COMPILE_DIR="/tmp"
 GIT_REPO="https://github.com/Azure/azure-signalr-bench.git"
 PROJECT_NAME="azure-signalr-bench"
 
 while [[ "$#" > 0 ]]; do
-    key="$1"
+  key="$1"
+  shift
+  case $key in
+  --version | -v)
+    VERSION="$1"
     shift
-    case $key in
-    --version | -v)
-        VERSION="$1"
-        shift
-        ;;
-    --Pod | -p)
-        Pod="$1"
-        shift
-        ;;
-    *)
-        echo "ERROR: Unknow argument '$key'" 1>&2
-        print_usage
-        exit -1
-        ;;
-    esac
+    ;;
+  --pod | -p)
+    POD="$1"
+    shift
+    ;;
+  *)
+    echo "ERROR: Unknow argument '$key'" 1>&2
+    print_usage
+    exit -1
+    ;;
+  esac
 done
 
-function publish() {
-  cd $COMPILE_DIR/$PROJECT_NAME/src/Pods/$Pod
-  sudo rm -rf publish || true
-  echo "start to publish $Pod"
-  dotnet publish -r linux-x64 -c release -o publish /p:useapphost=true
-  dotnet restore
-  cd publish && zip -r ${Pod}.zip *
-  echo "create dir:$Pod"
+function throw_if_empty() {
+    local name="$1"
+    local value="$2"
+    if [[ -z "$value" ]]; then
+        echo "ERROR: Parameter $name cannot be empty." 1>&2
+        exit -1
+    fi
 }
 
-# cd compile dir
-cd $COMPILE_DIR
+function replace() {
+  local placeholder=$1
+  local content=$2
+  content=$(echo "${content////\\/}")
+  sed -e "s/$placeholder/$content/g"
+}
+
+function publish() {
+  cd $COMPILE_DIR/$PROJECT_NAME/src/Pods/$POD
+  sudo rm -rf publish_${VERSION} || true
+  echo "start to publish $POD"
+  dotnet publish  ${POD}_${VERSION}.csproj -r linux-x64 -c release -o publish /p:useapphost=true
+  cd publish_${VERSION} && zip -r ${Pod}_${VERSION}.zip *
+  echo "create dir:$POD"
+}
+
+throw_if_empty "Pod" $POD
+throw_if_empty "Version" $Version
 LATEST_COMMIT_HASH=$(git ls-remote $GIT_REPO v2 | awk '{print $1 }')
 echo $LATEST_COMMIT_HASH
-CURRENT_COMMIT_HASH=$(cat variable_current_hash )  || true
+CURRENT_COMMIT_HASH=$(cat $COMPILE_DIR/variable_current_hash) || true
 echo $CURRENT_COMMIT_HASH
-if [[ $LATEST_COMMIT_HASH == $CURRENT_COMMIT_HASH ]];then
+
+if [[ $LATEST_COMMIT_HASH == $CURRENT_COMMIT_HASH ]]; then
   echo "Already latest commit"
 else
-  echo "Remove project"
-#  sudo rm -rf $PROJECT_NAME || true
-  echo "cloning project"
- # git clone $GIT_REPO
-  cd $COMPILE_DIR/$PROJECT_NAME
-  git checkout v2  
+  sudo rm -rf $COMPILE_DIR/$PROJECT_NAME
+  git clone $GIT_REPO
+  git checkout v2
+  echo $LATEST_COMMIT_HASH > $COMPILE_DIR/variable_current_hash
 fi
-
-cd $COMPILE_DIR/$PROJECT_NAME/src/Pods/$Pod
-case $Pod in 
-  "Client")
-    echo "build Client"
-    KEY=$(cat $Pod.csproj | grep "Microsoft.AspNet.SignalR.Client" | awk '{print $2 " " $3'
   
 
+cd $COMPILE_DIR/$PROJECT_NAME/src/Pods/$POD
+
+if [[ -f $COMPILE_DIR/$PROJECT_NAME/src/Pods/$POD/public_${VERSION}/${POD}_${VERSION}.zip ]];then
+  echo "zip file already exist"
+  exit 0
+fi 
 
 
+case $POD in
+"Client")
+  echo "build Client"
+  KEY=$(cat $POD.csproj | grep "Microsoft.AspNet.SignalR.Client" | awk '{print $2 " " $3}')
+  echo $KEY
+  cat $POD.csproj | replace "$KEY" "Include=\"Microsoft.AspNet.SignalR.Client\" Version=\"${VERSION}\"" | replace "$KEY" "Include=\"Microsoft.AspNetCore.SignalR.Client\" Version=\"${VERSION}\"" > ${POD}_${VERSION}.csproj
+  publish
+  ;;
+esac
