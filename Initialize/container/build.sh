7364009c97eca429b2f0abdc03627135ff14c836
Binjie Qian <biqian@microsoft.com>
2022-06-12 14:23:45 +0800

    update build.sh: move to mcr update WpsUpstream: fix conflict

--- Initialize/container/build.sh
+++ Initialize/container/build.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-docker build - < ./base_image.docker -t signalrbenchmark/perf:1.4.4
+#docker build - < ./base_image.docker -t signalrbenchmark/perf:1.4.4
+docker build - < ./base_image.docker -t public/signalrbenchmark/perf:1.0.0
