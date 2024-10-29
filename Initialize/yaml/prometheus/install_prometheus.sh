035cbb4de0ed63b55330873cabb01417546bb8be
biqian <biqian@microsoft.com>
2024-10-29 16:13:37 +0800

    Switch to first party image

--- Initialize/yaml/prometheus/install_prometheus.sh
+++ Initialize/yaml/prometheus/install_prometheus.sh
@@ -3,8 +3,8 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-helm install prometheus prometheus-community/kube-prometheus-stack -f values.yml
-#helm upgrade prometheus prometheus-community/kube-prometheus-stack -f values.yml
+#helm install prometheus prometheus-community/kube-prometheus-stack -f values.yml --version 61.3.2
+#helm upgrade prometheus prometheus-community/kube-prometheus-stack -f values.yml --version 61.3.2
 
-#helm uninstall prometheus
+helm uninstall prometheus
 
